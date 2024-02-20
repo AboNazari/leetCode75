320cb4b034037a521df04881fcb36f7c965d3c01
AboNazari <abdullahnazari2001@gmail.com>
2024-02-20 19:14:34 +0600

    insert delete added to linked list

--- ds/linkedList.cpp
+++ ds/linkedList.cpp
@@ -4,44 +4,89 @@ using namespace std;
 
 struct Node {
     int data;
-    Node* link;
+    Node *next;
 };
 
-void appendNode(Node** head, int newData){
+void add(Node **head, int newData) {
+//    allocate an empty node in memory and assign new data to it
+    Node *newNode = new Node();
+    newNode->data = newData;
+    newNode->next = nullptr;
 
-    Node* newNode = new Node();
-    newNode -> data = newData;
-    newNode -> link = nullptr;
-
-    if(*head == nullptr){
+    if (*head == nullptr) {
         *head = newNode;
         return;
     }
 
-    Node* last = *head;
-    while (last -> link != nullptr){
-        last  = last -> link;
+    Node *last = *head;
+    while (last->next != nullptr) {
+        last = last->next;
     }
-    last -> link = newNode;
+    last->next = newNode;
 
 }
 
+void insert(Node **head, int newData, int n) {
+    Node *newNode = new Node();
+    newNode->data = newData;
+    newNode->next = nullptr;
 
-int main(){
-    Node* A;
-    A = nullptr;
-
-    appendNode(&A, 2);
-    appendNode(&A, 4);
-    appendNode(&A, 6);
-
-
-    Node* temp = A;
-    cout << "List of Elements in list: " << endl;
-
-    while (temp != nullptr){
-        cout << temp -> data << endl;
-        temp = temp -> link;
+    if (n == 1) {
+        newNode->next = *head;
+        *head = newNode;
+        return;
     }
+
+    Node *last = *head;
+    for (int i = 0; i < n - 2; i++) {
+        last = last->next;
+    }
+    newNode->next = last->next;
+    last->next = newNode;
+
+}
+
+void print(Node *head) {
+    Node *temp = head;
+    while (temp != nullptr) {
+        cout << temp->data;
+        cout << "  ";
+        temp = temp->next;
+    }
+}
+
+void deleteNode(Node **head, int position) {
+    Node *temp = *head;
+    Node *temp2;
+
+    if (position == 1) {
+        *head = temp->next;
+        delete temp;
+        return;
+    }
+
+    for (int i = 0; i < position - 1; i++) {
+        temp2 = temp;
+        temp = temp->next;
+    }
+    temp2->next = temp->next;
+    delete temp;
+}
+
+int main() {
+    Node *head;
+    head = nullptr;
+
+    add(&head, 2);
+    add(&head, 5);
+    add(&head, 6);
+    insert(&head, 8, 2); // 2 8 5 6
+    insert(&head, 10, 1); // 10 2 8 5 6
+    cout << "List of Elements in list: " << endl;
+    print(head);
+
+    deleteNode(&head, 1);
+    cout << "List of Elements in list: " << endl;
+    print(head); // 10 2 8 5 6
 
 }
